897ad82eb8168417ba3fe8e6f00ac400ee27ca79
Lans12345 <olanalans12345@gmail.com>
2026-04-01 09:17:58 +0800

    updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,14 +36,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "2c944c96fa852b5132bf4ed3e61c6cd4
 "favicon.ico": "854039ff68c9f1b62604811a3f684818",
 "favicon.png": "854039ff68c9f1b62604811a3f684818",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "8ade7e3d4e3d95bd1582fd01d049542e",
+"flutter_bootstrap.js": "2418c17856210bc0467cb7154e4f10c7",
 "icons/Icon-192.png": "b599ac4cd4f34f1ff721bd5d34b5ae3b",
 "icons/Icon-512.png": "b599ac4cd4f34f1ff721bd5d34b5ae3b",
 "icons/Icon-maskable-192.png": "b599ac4cd4f34f1ff721bd5d34b5ae3b",
 "icons/Icon-maskable-512.png": "b599ac4cd4f34f1ff721bd5d34b5ae3b",
 "index.html": "fa3c608b840f0d4b44cbe78e405cbcdf",
 "/": "fa3c608b840f0d4b44cbe78e405cbcdf",
-"main.dart.js": "04d2a41110c0a05afb1fef48ed97e49d",
+"main.dart.js": "df49d642c765ceb0fe0753eeaa08c7f0",
 "manifest.json": "10dfccfbd8a77467cede2fb8bebab22a",
 "version.json": "020c8b68e3d4f9e28defdb3717a364a9"};
 // The application shell files that are downloaded before a service worker can
